327cad199e4a099393ab8657a143974c1c56d62a
Ibra <ibraheeme@yahoo.com>
2022-10-17 20:49:18 -0400

    Adding more Javadoc

--- src/IbrasFeature.java
+++ src/IbrasFeature.java
@@ -2,6 +2,25 @@
  * This branch will contain code necessary for Ibraheem Elmasri of Group 7's feature which is book details
  */
 
+/**
+ * The following is a list of requirements listed for the book details feature:
+ *
+ * An administrator must be able to create a book with the book ISBN, book name, book description, price, author,
+ * genre, publisher , year published and copies sold.
+ *
+ * Must be able retrieve a book’s details by the ISBN
+ *
+ * An administrator must be able to create an author with first name, last name, biography and publisher
+ *
+ * Must be able to retrieve a list of books associate with an author
+ */
 
 public class IbrasFeature {
+
+
+
+
+
+
+
 }
